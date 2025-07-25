453821cbbc330def2476369246ed9c1247947335
robert-bryson <rsmbryson@gmail.com>
2025-07-25 11:22:35 -0700

    Added initial flights, updated globe

--- src/pages/Flights.tsx
+++ src/pages/Flights.tsx
@@ -1,9 +1,12 @@
 import { useRef } from 'react';
-import { Viewer, CameraFlyTo, GeoJsonDataSource} from 'resium';
-import { Cartesian3, Color} from 'cesium';
+import { Viewer, CameraFlyTo,ImageryLayer, GeoJsonDataSource} from 'resium';
+import { Cartesian3, Color, Ion, UrlTemplateImageryProvider} from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 const Flights = () => {
+    // Optional: disable Cesium Ion access token if unused
+    Ion.defaultAccessToken = ''
+
     const viewerRef = useRef(null)
 
     return (
@@ -12,8 +15,9 @@ const Flights = () => {
                 full 
                 ref={viewerRef}
                 baseLayerPicker={false}
-                timeline={false}
                 animation={false}
+                timeline={false}
+                skyAtmosphere={false}
             >
                 <CameraFlyTo
                     duration={3}
@@ -26,6 +30,14 @@ const Flights = () => {
                     strokeWidth={2}
                     fill={Color.PURPLE.withAlpha(0.3)}
                 />
+                <ImageryLayer
+                    imageryProvider={new UrlTemplateImageryProvider({
+                        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+                        subdomains: ['a', 'b', 'c', 'd'],
+                        credit: '© CARTO',
+                    })}
+                />
+
             </Viewer>
         </section>
     )
