c376ae9faa2386814c71ceafa5b094f76ccc088e
robert-bryson <rsmbryson@gmail.com>
2026-02-24 07:19:52 -0800

    fixup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 import remarkFrontmatter from 'remark-frontmatter'
